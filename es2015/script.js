dbc7b2672e1ecf948c431bdb8c252553120a5b59
Roman <9malabar6@mail.ru>
2020-10-23 00:07:20 +0300

    lector 3

---
+++ es2015/script.js
@@ -0,0 +1,21 @@
+"use strict";
+
+let num1 = 12;
+let num2 = 8;
+
+
+console.log('+', num1 + num2);
+console.log('-', num1 - num2);
+console.log('*', num1 * num2);
+console.log('/', num1 / num2);
+
+
+const str1 = 'Hello ';
+const str2 = 'world';
+
+console.log('+', str1 + str2);
+
+console.log ('1 + 2',1 + '2');
+
+console.log('True + 1', true + 1);
+console.log('false + 1', false + 1);
